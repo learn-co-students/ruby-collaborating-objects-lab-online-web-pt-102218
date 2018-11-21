7e6013b6de076d75a7d9b823bdc5134ce0e603ee
Linda Chang <lindachang186@gmail.com>
2018-11-21 17:44:32 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,19 @@
+require "pry"
+
+class Artist
+
+  attr_accessor :name
+  @@songs = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def add_song(song)
+    #binding.pry
+    @@songs << song
+    song.artist = self
+
+  end
+
+end
