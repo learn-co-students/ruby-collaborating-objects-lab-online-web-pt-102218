a54cd286f5e9edd83be11502d309295c759e7b28
Johnathon Wood <jaywood128@gmail.com>
2018-11-21 18:07:35 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,12 +2,14 @@ require 'pry'
 class Artist
 
   attr_accessor :name, :songs
+
   @@all = []
   def initialize(name)
     @name = name
     @songs = []
   #  @@all << self
   end
+
   def add_song(song)
     @songs << song
   end
