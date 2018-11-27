faeb9125a1c6d34fbeb3fc606cb537fd0ba57c2e
Linda Chang <lindachang186@gmail.com>
2018-11-27 22:16:43 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,13 +24,8 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    found_artist = self.all.find{|artist| artist.name == name}
-
-    if found_artist
-      found_artist
-    else
-      Artist.new(name)
-    end
+    self.all.find{|artist| artist.name == name} ? self.all.find{|artist| artist.name == name} : self.new(name).tap {|artist| artist.save}
+    #self.find(name) ? self.find(name) : self.create(name) You could have done this if you had a find and a create method separately
   end
 
   def print_songs
@@ -38,5 +33,7 @@ class Artist
       puts song.name
     end
   end
+
+
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -5,11 +5,28 @@ class Song
     @name = name
   end
 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+
+
   def self.new_by_filename(filename)
-  #binding.pry
-   song = self.new(filename)
-   song.name = filename.split(" - ")[1]
-   song
+   artist, song = filename.split(" - ")
+   new_song = self.new(song)
+   #song = self.new(filename.split(" - ")[1])
+   #artist = filename.split(" - ")[0]
+   new_song.artist_name = artist
+   #song.artist = filename.split(" - ")[0]
+   new_song
  end
 
 end
+
+#song = filename.split(" - ")[1]
+#binding.pry
+#artist = filename.split(" - ")[0]
+#new_song = self.new(song)
+#new_song.artist_name = artist
+#song.artist = filename.split(" - ")[0]
+#song
