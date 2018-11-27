cf9f43762d4f2ec8d25eae8e188e02db53c404fa
Linda Chang <lindachang186@gmail.com>
2018-11-27 17:28:17 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,20 +2,41 @@ require "pry"
 
 class Artist
 
-  attr_accessor :name, :song
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
 
   def initialize(name)
     @name = name
-    @song = []
+    @songs = []
   end
 
   def add_song(song)
-    self.song = song
-    self
+    @songs << song
   end
 
   def save
-
+    @@all << self
   end
+
+  def self.find_or_create_by_name(name)
+    found_artist = self.all.find{|artist| artist.name == name}
+
+    if found_artist
+      found_artist
+    else
+      Artist.new(name)
+    end
+    #if self.all.find do |artist|
+    #  artist.name == name
+    #  name
+    #else Artist.new(name)
+    #end
+  
+end
 
 end
